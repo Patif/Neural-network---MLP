627898709e2d636d625515dc057994403974a626
Patrycja Frąckiewicz <65953083+Patif@users.noreply.github.com>
2021-11-10 15:25:10 +0100

    Test v2

--- main.py
+++ main.py
@@ -15,6 +15,18 @@ if __name__ == "__main__":
     reps = 10
 
     acc = 0
+    neural_structure = [(N, softplus),
+                        (M, softmax),
+                        [M_prim]]
+    for i in range(reps):
+        network = MLP(neural_structure)
+        network.train(X_train, Y_train, 100, 2.5, X_test[0:9500], Y_test[0:9500])
+        acc_i = network.assessment(X_test[9500:], Y_test[9500:])
+        print(i, acc_i)
+        acc += acc_i
+    print(acc / reps, "softplus")
+
+    acc = 0
     neural_structure = [(N, tanh),
                         (M, softmax),
                         [M_prim]]
@@ -26,17 +38,6 @@ if __name__ == "__main__":
         acc += acc_i
     print(acc / reps, "tanh")
 
-    acc = 0
-    neural_structure = [(N, sigmoid),
-                        (M, softplus),
-                        [M_prim]]
-    for i in range(reps):
-        network = MLP(neural_structure)
-        network.train(X_train, Y_train, 100, 2.5, X_test[0:9500], Y_test[0:9500])
-        acc_i = network.assessment(X_test[9500:], Y_test[9500:])
-        print(i, acc_i)
-        acc += acc_i
-    print(acc / reps, "softplus")
 
     acc = 0
     neural_structure = [(N, sigmoid),
